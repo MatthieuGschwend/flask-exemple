d1ffc9e79d13bf69f3a0198ab60cccedb4b39240
MatthieuGschwend <89905119+MatthieuGschwend@users.noreply.github.com>
2022-11-13 12:46:20 +0100

    Add files via upload

--- application.py
+++ application.py
@@ -1,25 +1,7 @@
-import pickle
-import flask
-from flask import Flask, jsonify, request
-app = flask.Flask(__name__) 
-import numpy as np
-#loading a model from a file called model.pkl
-model = pickle.load(open("model.pkl","rb"))
+from flask import Flask
 
+application = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-
- #getting an array of features from the post request's body
- query_parameters = request.args
- feature_array = np.fromstring(query_parameters['feature_array'],dtype=float,sep=",")
-
- #creating a response object
- #storing the model's prediction in the object
- response = {}
- response['predictions'] = model.predict_proba([feature_array]).tolist()
-
- #returning the response object as json
- return flask.jsonify(response)
-if __name__ == "__main__":
- app.run(debug=True)
+@application.route('/')
+def hello_world():
+    return 'Sup. Suboerzo'
