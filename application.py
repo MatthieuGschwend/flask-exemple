0cd1f656ac38eeaeda1d9987c3d45acbd8e3f877
MatthieuGschwend <89905119+MatthieuGschwend@users.noreply.github.com>
2022-11-13 15:29:46 +0100

    Update application.py

--- application.py
+++ application.py
@@ -10,3 +10,18 @@ model = pickle.load(open("model.pkl","rb"))
 @application.route('/')
 def hello_world():
     return 'Sup. Suboerzo'
+
+@application.route('/predict', methods=['POST'])
+def predict():
+
+ #getting an array of features from the post request's body
+ query_parameters = request.args
+ feature_array = np.fromstring(query_parameters['feature_array'],dtype=float,sep=",")
+
+ #creating a response object
+ #storing the model's prediction in the object
+ response = {}
+ response['predictions'] = model.predict_proba([feature_array]).tolist()
+
+ #returning the response object as json
+ return flask.jsonify(response)
