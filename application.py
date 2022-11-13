ea88890ab22c4529a3bf33e893bdc48be99a044b
MatthieuGschwend <89905119+MatthieuGschwend@users.noreply.github.com>
2022-11-13 15:19:10 +0100

    Update application.py

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ import numpy as np
 import xgboost
 application = Flask(__name__)
 #loading a model from a file called model.pkl
-#model = pickle.load(open("model.pkl","rb"))
+model = pickle.load(open("model.pkl","rb"))
 
 @application.route('/')
 def hello_world():
