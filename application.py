4f341d4b0ef7241ad6cb462a5e8715b74f40d970
MatthieuGschwend <89905119+MatthieuGschwend@users.noreply.github.com>
2022-11-13 15:12:22 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 #import pickle
-import flask
+
 from flask import Flask, jsonify, request
 #import numpy as np
 #import xgboost
-app = flask.Flask(__name__) 
+application = Flask(__name__)
 #loading a model from a file called model.pkl
 #model = pickle.load(open("model.pkl","rb"))
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return 'Sup. Suboerzo'
