6589ace699e6b98de0c5408ca6fe0da52e5c7034
MatthieuGschwend <89905119+MatthieuGschwend@users.noreply.github.com>
2022-11-13 17:50:14 +0100

    Add files via upload

--- application.py
+++ application.py
@@ -1,11 +1,13 @@
 import pickle
-
+import flask
 from flask import Flask, jsonify, request
 import numpy as np
 import xgboost
 application = Flask(__name__)
 #loading a model from a file called model.pkl
-model = pickle.load(open("model.pkl","rb"))
+model = pickle.load(open('model.pkl','rb'))
+
+
 
 @application.route('/')
 def hello_world():
@@ -13,17 +15,9 @@ def hello_world():
 
 @application.route('/okpath')
 def hello_world2():
-    model = pickle.load(open("model.pkl","rb"))
-    feature_array = [ 1.00000000e+00,  0.00000000e+00,  0.00000000e+00,  2.00000000e+00,
-        2.00000000e+00,  1.00000000e+00, -9.46100000e+03, -2.12000000e+03,
-        0.00000000e+00,  0.00000000e+00,  1.00000000e+00, -1.13400000e+03,
-        2.62948593e-01,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
-        0.00000000e+00,  0.00000000e+00,  7.50000000e-01,  2.50000000e-01,
-       -1.43700000e+03, -1.03000000e+02, -8.74000000e+02, -4.99875000e+02,
-       -6.37000000e+02,  1.39375780e-01, -1.43700000e+03, -4.76000000e+02,
-       -9.74500000e+02, -6.61333333e+02]
+    
 
-    return str(type(load_model))
+    return 'Sup. Suboerzo'
 
 @application.route('/predict', methods=['POST','GET'])
 def predict():
@@ -49,3 +43,7 @@ def predict():
     
  return flask.jsonify(response)
  #return query_parameters['feature_array']
+
+
+if __name__ == "__main__":
+ application.run(debug=True)
