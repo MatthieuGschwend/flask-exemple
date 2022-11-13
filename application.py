176322c99c7ad2101531cf35deb97d599c7de190
MatthieuGschwend <89905119+MatthieuGschwend@users.noreply.github.com>
2022-11-13 15:16:25 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
-#import pickle
+import pickle
 
 from flask import Flask, jsonify, request
-#import numpy as np
-#import xgboost
+import numpy as np
+import xgboost
 application = Flask(__name__)
 #loading a model from a file called model.pkl
 #model = pickle.load(open("model.pkl","rb"))
