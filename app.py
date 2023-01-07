aa60b29e8cc4d067178ea6284e60f603ca0e3373
MatthieuGschwend <89905119+MatthieuGschwend@users.noreply.github.com>
2023-01-07 17:07:00 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -45,5 +45,22 @@ def predict():
  return flask.jsonify(response)
  #return query_parameters['feature_array']
  
+@app.route('/predictproba', methods=['GET', 'POST'])
+def predict_proba():
+
+ #getting an array of features from the post request's body
+ query_parameters = request.args
+ feature_array = np.fromstring(query_parameters['feature_array'],dtype=float,sep=",")
+
+ #creating a response object
+ #storing the model's prediction in the object
+ response = {}
+ response['predictions'] = model.predict_proba([feature_array]).tolist()
+
+ #returning the response object as json
+    
+ return flask.jsonify(response)
+ #return query_parameters['feature_array']
+ 
  if __name__ == "__main__":
     app.run(debug=True)
